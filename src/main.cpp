17bf121e1fe0a24770f1593e9d2b9d76f951ff48
CharlotteNoraJones <charlottenorajones@gmail.com>
2024-08-19 19:00:24 -0500

    Played around with components.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,39 @@
 #include <stdio.h>
+#include <memory>
 
 #include "flecs.h"
+#include "raylib.h"
+
+struct Velocity
+{
+    Vector2 velocity;
+};
+
+struct Position
+{
+    Vector2 position;
+};
 
 int main()
 {
     flecs::world world;
 
-    flecs::entity e = world.entity("Bob");
+    flecs::entity e = world.entity();
 
-    printf("Entity Name: %s\n", e.name().c_str());
+    e.add<Velocity>();
 
-    flecs::entity bobAgain = world.lookup("Bob");
+    e.set<Position>({.position{10, 20}})
+        .set<Velocity>({.velocity{1, 2}});
 
-    printf("Another bob's Name: %s\n", bobAgain.name().c_str());
+    const std::reference_wrapper<const Position> p = std::ref(*e.get<Position>());
+
+    printf("position: %f %f\n", p.get().position.x, p.get().position.y);
+
+    e.remove<Position>();
+
+    flecs::entity pos_e = world.entity<Position>();
+    const EcsComponent *c = pos_e.get<flecs::Component>();
+
+    printf("Name: %s\n", pos_e.name().c_str());
+    printf("Component Size: %d\n", c->size);
 }
